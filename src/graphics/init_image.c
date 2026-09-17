2645dcf62aecc0dd846560307197a0521a5e0f80
kaide-lucas <76270182+kaide-lucas@users.noreply.github.com>
2026-09-17 00:38:17 +0000

    Adds texture loadin function and minor changes to the header.

--- src/graphics/init_image.c
+++ src/graphics/init_image.c
@@ -6,7 +6,7 @@
 /*   By: kaidda-s <kaidda-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/05 23:36:26 by kaidda-s          #+#    #+#             */
-/*   Updated: 2026/09/06 00:02:38 by kaidda-s         ###   ########.fr       */
+/*   Updated: 2026/09/16 03:06:13 by kaidda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,15 @@ int	init_image(t_game *game)
 		return (1);
 	}
 	return (0);
+}
+
+static int load_texture(void *mlx_ptr, t_image *img, char *path) {
+    img->ptr = mlx_xpm_file_to_image(mlx_ptr, path, &img->width, &img->height);
+    if (!img->ptr) 
+        return (1);
+    img->addr = mlx_get_data_addr(img->ptr, &img->bits_per_pixel,
+            &img->line_length, &img->endian);
+    if (!img->addr)
+        return (1);
+    return (0);
 }
